024e0c7c33d4728196d7e9bf9a8eab9250668e3b
Connor Weida <cjweida@gmail.com>
2025-05-09 22:50:29 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-nfl = pd.read_csv("league_rankings/nfl_rankings.csv")
+# Sample title
+st.title("Major Sports League Rankings")
+
+# Sidebar dropdown to select plot
+plot_choice = st.selectbox(
+    "Choose a League:",
+    ["NFL", "MLB", "NBA", "NHL", "MLS"]
+)
+
+league = pd.read_csv(f'league_rankings/{plot_choice.lower()}_rankings.csv')
 
 fig, ax = plt.subplots()
 ax.axis('off')
@@ -14,6 +23,19 @@ table = ax.table(cellText=nfl.values, colLabels=nfl.columns, loc='center')
 header_color = '#2C3E50'      # Deep navy blue
 cell_color = '#ECF6FD'        # Very light blue
 header_text_color = 'white'
+
+# Apply colors
+for (row, col), cell in table.get_celld().items():
+    if row == 0:  # Header row
+        cell.set_facecolor(header_color)
+        cell.get_text().set_color(header_text_color)
+        cell.set_text_props(weight='bold')  # Optional: bold header text
+    else:
+        cell.set_facecolor(cell_color)
+
+st.subheader(f'{plot_choice}')
+st.pyplot(fig)
+
 
 # header_color = '#3A6B35'      # Forest green
 # cell_color = '#E9F7EF'        # Mint cream
@@ -31,14 +53,7 @@ header_text_color = 'white'
 # cell_color = '#F2F4F4'        # Soft gray
 # header_text_color = 'white'
 
-# Apply colors
-for (row, col), cell in table.get_celld().items():
-    if row == 0:  # Header row
-        cell.set_facecolor(header_color)
-        cell.get_text().set_color(header_text_color)
-        cell.set_text_props(weight='bold')  # Optional: bold header text
-    else:
-        cell.set_facecolor(cell_color)
 
 
-st.pyplot(fig)
+
+
